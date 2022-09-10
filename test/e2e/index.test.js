38704b3da0d7d1087d8948b536de69e7f2b0e0a1
AnthonyLzq <sluzquinosa@uni.pe>
2022-09-09 20:37:24 -0500

    clase 10 - 2/2

--- test/e2e/index.test.js
+++ test/e2e/index.test.js
@@ -1,8 +1,8 @@
 const axios = require('axios')
-
+const { faker } = require('@faker-js/faker')
 const { server } = require('../../src/expressExample/network')
 
-const URL = `http://localhost:${process.env.PORT || 1996}/`
+const URL = `http://localhost:${process.env.PORT || 1996}`
 
 beforeAll(async () => {
   await server.start()
@@ -25,4 +25,77 @@ describe('API: GET /', () => {
   })
 })
 
-describe.skip('E2E test: Use cases from UserService', () => {})
+describe('E2E test: Use cases from UserService', () => {
+  const name = faker.name.firstName()
+  const lastName = faker.name.lastName()
+  const newUser = {
+    name,
+    lastName,
+    email: faker.internet.email(name, lastName).toLowerCase(),
+    password: faker.datatype.string()
+  }
+  const tokens = {
+    accessToken: '',
+    refreshToken: ''
+  }
+  const users = []
+
+  describe('Testing save user', () => {
+    let response = {}
+
+    test('Should return 201 as status code', async () => {
+      response = await axios.post(`${URL}/api/user/signup`, newUser)
+      expect(response.status).toBe(201)
+      users.concat(response.data.message)
+    })
+  })
+
+  describe('Testing login a user', () => {
+    const keys = ['accessToken', 'refreshToken']
+
+    test('Should return accessToken and refreshToken', async () => {
+      const {
+        data: { message }
+      } = await axios.post(`${URL}/api/user/login`, {
+        email: newUser.email,
+        password: newUser.password
+      })
+
+      expect(Object.keys(message)).toEqual(keys)
+      tokens.accessToken = message.accessToken
+      tokens.refreshToken = message.refreshToken
+    })
+  })
+
+  describe('Testing get all users', () => {
+    test('Should return an array of users', async () => {
+      const {
+        data: { message: allUsers }
+      } = await axios.get(`${URL}/api/user`, {
+        headers: {
+          Authorization: `Bearer ${tokens.accessToken}`
+        }
+      })
+
+      expect(allUsers.some(u => u.email === newUser.email)).toBe(true)
+    })
+  })
+})
+
+/**
+ * E commerce
+ * ------
+ * 1. Registrar un usuario como cliente
+ *  1.1. Login del cliente
+ * 2. Recargar saldo del cliente
+ * 3. Registrar un usuario como vendedor
+ *  3.1. Login del vendedor
+ * 4. Registrar un artículo (precio) del vendedor
+ * 5. El cliente intenta comprar el artículo
+ *  5.1. El saldo del cliente del insuficiente -> Recarga más saldo
+ *  5.2. El saldo es suficiente -> Se genera la compra
+ * 6. El saldo pasa a la cuenta del vendedor
+ * 7. El artículo pasa a la cuenta del cliente
+ *
+ * Nota: las únicas rutas públicas son las rutas de registro
+ */
